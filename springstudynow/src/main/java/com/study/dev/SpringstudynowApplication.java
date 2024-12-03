b206d50ee3df05e948d3e010c236148b7ebd9719
ella <51068439+sunyoung-park@users.noreply.github.com>
2024-12-03 11:40:39 +0900

    add introActivity

---
+++ springstudynow/src/main/java/com/study/dev/SpringstudynowApplication.java
@@ -0,0 +1,13 @@
+package com.study.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudynowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstudynowApplication.class, args);
+	}
+
+}
---
+++ springstudynow/src/test/java/com/study/dev/SpringstudynowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudynowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
